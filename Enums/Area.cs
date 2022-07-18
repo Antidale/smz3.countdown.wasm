12141298556b61b1be78b0bc29239092de49bd80
antidale <antidale@gmail.com>
2022-07-17 18:20:47 -0700

    update areas, show all locations

--- Enums/Area.cs
+++ Enums/Area.cs
@@ -1,11 +1,9 @@
 ﻿using smz3.countdown.wasm.Attributes;
-using System.ComponentModel;
 
 namespace smz3.countdown.wasm.Enums
 {
     public enum Area
     {
-
         [Area("Castle Tower", "Castle Tower", "Castle Tower")]
         CastleTower,
 
@@ -15,44 +13,115 @@ namespace smz3.countdown.wasm.Enums
         [Area("Desert Palace", "Desert Palace", "Desert Palace")]
         DesertPalace,
 
+        [Area("Tower of Hera", "Tower of Hera", "Tower of Hera")]
         TowerOfHera,
 
+        [Area("Palace of Darkness", "Palace of Darkness", "Palace of Darkness")]
         PalaceOfDarkness,
-
+        
+        [Area("Swamp Palace", "Swamp Palace", "Swamp Palace")]
         SwampPalace,
+
+        [Area("Skull Woods", "Skull Woods", "Skull Woods")]
         SkullWoods,
+
+        [Area("Thieves' Town", "Thieves' Town", "Thieves' Town")]
         ThievesTown,
+
+        [Area("Ice Palace", "Ice Palace", "Ice Palace")]
         IcePalace,
+
+        [Area("Misery Mire", "Misery Mire", "Misery Mire")]
         MiseryMire,
+
+        [Area("Turtle Rock", "Turtle Rock", "Turtle Rock")]
         TurtleRock,
+
+        [Area("Ganon's Tower", "Ganon's Tower", "Ganon's Tower")]
         GanonsTower,
-        LightWorldDeathMountainWest,
+        
+        [Area("Light World", "Death Mountain", "Light World Death Mountain East")]
         LightWorldDeathMountainEast,
+        
+        [Area("Light World", "Death Mountain", "Light World Death Mountain West")]
+        LightWorldDeathMountainWest,
+        
+        [Area("Light World", "Light World", "Light World North West")]
         LightWorldNorthWest,
+        
+        [Area("Light World", "Light World", "Light World North East")]
         LightWorldNorthEast,
+        
+        [Area("Light World", "Light World", "Light World South")]
         LightWorldSouth,
+        
+        [Area("Hyrule Castle", "Hyrule Castle", "Hyrule Castle")]
         HyruleCastle,
+        
+        [Area("Dark World", "Death Mountain", "Dark World Death Mountain West")]
         DarkWorldDeathMountainWest,
+        
+        [Area("Dark World", "Death Mountain", "Dark World Death Mountain East")]
         DarkWorldDeathMountainEast,
+        
+        [Area("Dark World", "Dark World", "Dark World North West")]
         DarkWorldNorthWest,
-        DarkWorldNorthEastk,
+        
+        [Area("Dark World", "Dark World", "Dark World North East")]
+        DarkWorldNorthEast,
+        
+        [Area("Dark World", "Dark World", "Dark World South")]
         DarkWorldSouth,
+        
+        [Area("Dark World", "Dark World", "Dark World Mire")]
         DarkWorldMire,
+
+        [Area("Crateria", "Crateria", "Crateria West")]
         CrateriaWest,
+
+        [Area("Crateria", "Crateria", "Crateria Central")]
         CrateriaCentral,
+        
+        [Area("Crateria", "Crateria", "Crateria East")]
         CrateriaEast,
+
+        [Area("Brinstar", "Brinstar", "Brinstar Blue")]
         BrinstarBlue,
+
+        [Area("Brinstar", "Brinstar", "Brinstar Green")]
         BrinstarGreen,
+
+        [Area("Brinstar", "Brinstar", "Brinstar Pink")]
         BrinstarPink,
+
+        [Area("Brinstar", "Brinstar", "Brinstar Red")]
         BrinstarRed,
+        
+        [Area("Brinstar", "Brinstar", "Brinstar Kraid")]
         BrinstarKraid,
+
+        [Area("Wrecked Ship", "Wrecked Ship", "Wrecked Ship")] 
         WreckedShip,
+
+        [Area("Maridia", "Maridia", "Maridia Outer")] 
         MaridiaOuter,
+
+        [Area("Maridia", "Maridia", "Maridia Inner")] 
         MaridiaInner,
+
+        [Area("Upper Norfair", "Upper Norfair", "Norfair Upper West")]
         NorfairUpperWest,
+
+        [Area("Upper Norfair", "Upper Norfair", "Norfair Upper East")]
         NorfairUpperEast,
+
+        [Area("Upper Norfair", "Upper Norfair", "Norfair Upper Crocomire")] 
         NorfairUpperCrocomire,
+
+        [Area("Lower Norfair", "Lower Norfair", "NorfairLowerWest")]
         NorfairLowerWest,
+        
+        [Area("Lower Norfair", "Lower Norfair", "Norfair Lower East")]
         NorfairLowerEast
     }
 }
