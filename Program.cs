a1814a7a64d2b723d209734c551efcdca855dde8
antidale <antidale@gmail.com>
2022-07-18 20:44:34 -0700

    Updating Nav, adding location info

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using smz3.countdown.wasm;
+using smz3.countdown.wasm.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddSingleton<ItemService>();
+builder.Services.AddSingleton<LocationService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
---
+++ Services/ItemService.cs
@@ -0,0 +1,20 @@
+﻿using smz3.countdown.wasm.Enums;
+using smz3.countdown.wasm.Extenstions;
+
+namespace smz3.countdown.wasm.Services
+{
+    public class ItemService
+    {
+        public HashSet<string> Items { get; set; } = new HashSet<string>();
+
+        public ItemService()
+        {
+            var items = Enum.GetValues(typeof(Item));
+            Items = ((Item[])items).Select(x => new { Name = x.GetDescription(), IsMajor = x.IsMajor() })
+                                      .Where(x => x.IsMajor)
+                                      .Select(x => x.Name)
+                                      .ToHashSet();
+        }
+
+    }
+}
