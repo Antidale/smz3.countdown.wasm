508f209b8eb24b5ff6c16c45be42080c3d3bcee4
antidale <antidale@gmail.com>
2022-07-19 10:35:05 -0700

    adding the ability to configure what items you consider major

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using smz3.countdown.wasm;
@@ -7,6 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddSingleton<ItemService>();
 builder.Services.AddSingleton<LocationService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
