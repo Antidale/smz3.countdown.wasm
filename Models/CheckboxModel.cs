44d1107e96a1c7847f2cfa79f7f02b6d472b76fe
antidale <antidale@gmail.com>
2022-07-20 23:23:50 -0700

    rename Majors to Items, rework saving/tracking data

---
+++ Models/CheckboxModel.cs
@@ -0,0 +1,8 @@
+﻿namespace smz3.countdown.wasm.Models
+{
+    public class CheckboxModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public bool IsChecked { get; set; }
+    }
+}
