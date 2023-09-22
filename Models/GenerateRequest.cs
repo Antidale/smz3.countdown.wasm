a7108a59ae411e2f6a1e8924a04754e148876de6
Scott W <antidale@gmail.com>
2023-09-22 04:02:19 -0700

    stuff for the PYP tourney and in-progress work

---
+++ Models/GenerateRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace smz3.countdown.api.Models
+{
+    public class GenerateRequest
+    {
+        public string GameMode { get; set; } = "Normal";
+        public string Z3Logic { get; set; } = "Normal";
+        public string SMLogic { get; set; } = "Normal";
+        public string SwordLocation { get; set; } = "Randomized";
+        public string MorphLocation { get; set; } = "Normal";
+        public string Goal { get; set; } = "fastganondefeatmotherbrain";
+        public string KeyShuffle { get; set; } = "None";
+        public bool Race { get; set; } = false;
+        public string OpenTower { get; set; } = "SevenCrystals";
+        public string GanonVulnerable { get; set; } = "SevenCrystals";
+        public string OpenTourian { get; set; } = "FourBosses";
+        public string InitialItems { get; set; } = "Boots";
+    }
+}
